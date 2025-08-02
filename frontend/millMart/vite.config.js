d5cb4b4e15612e8791c02e77c30a2dd7cd3877f1
Vasu-B6747 <boyavasu7811@gmail.com>
2025-08-02 16:20:04 +0530

    fix: removed direct script include to prevent MIME errors and vite.config

--- frontend/millMart/vite.config.js
+++ frontend/millMart/vite.config.js
@@ -1,11 +1,4 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
 
-// export default defineConfig({
-//   base: './', 
-//   plugins: [react(),],
-// });
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
